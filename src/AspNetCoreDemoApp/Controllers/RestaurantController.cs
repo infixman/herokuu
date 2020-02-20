c136d4b6cd8d043d883d7d288c01b5527be375c0
infixman <infixman@gmail.com>
2020-02-20 22:14:13 +0800

    modify controller to use logic

--- src/AspNetCoreDemoApp/Controllers/RestaurantController.cs
+++ src/AspNetCoreDemoApp/Controllers/RestaurantController.cs
@@ -1,3 +1,4 @@
+using AspNetCoreDemoApp.Logic;
 using AspNetCoreDemoApp.Model;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -7,15 +8,18 @@ namespace AspNetCoreDemoApp.Controllers
     [Route("api/[controller]")]
     public class RestaurantController : ControllerBase
     {
+        private RestaurantLogic _logic;
+
+        public RestaurantController(AppDbContext dbContext)
+        {
+            _logic = new RestaurantLogic(dbContext);
+        }
 
         [HttpGet]
         public IEnumerable<JapanRestaurant> Get(string week, string type, string star,
             bool parking, bool uber, bool deposit, string position, int page, int limit)
         {
-            var result = new List<JapanRestaurant>();
-
-
-            return result;
+            return _logic.GetRestaurant(week, type, star, parking, uber, deposit, position, page, limit);
         }
     }
 }
