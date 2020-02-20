35916cc08441b77b340597dc7f1cd980bbf195c9
infixman <infixman@gmail.com>
2020-02-20 21:44:47 +0800

    add JapanRestaurant model

---
+++ src/AspNetCoreDemoApp/Model/JapanRestaurant.cs
@@ -0,0 +1,22 @@
+namespace AspNetCoreDemoApp.Model
+{
+    public class JapanRestaurant
+    {
+        public string Name { get; set; }
+        public string W1Time { get; set; }
+        public string W2Time { get; set; }
+        public string W3Time { get; set; }
+        public string W4Time { get; set; }
+        public string W5Time { get; set; }
+        public string W6Time { get; set; }
+        public string W7Time { get; set; }
+        public string Type { get; set; }
+        public string Star { get; set; }
+        public bool Parking { get; set; }
+        public bool Uber { get; set; }
+        public bool Deposit { get; set; }
+        public double Score { get; set; }
+        public string Position { get; set; }
+
+    }
+}
